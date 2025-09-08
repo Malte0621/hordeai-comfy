d5c9dd96b2b752795bc9fb2dffbb225a658a0c13
Malte0621 <50240920+Malte0621@users.noreply.github.com>
2025-09-08 18:55:53 +0200

    Bump version to 0.0.2.

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ __all__ = [
 
 __author__ = """Malte0621"""
 __email__ = "yt.malte0621@gmail.com"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from .src.hordeai.nodes import NODE_CLASS_MAPPINGS
 from .src.hordeai.nodes import NODE_DISPLAY_NAME_MAPPINGS
